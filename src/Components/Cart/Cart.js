62039880304434dfd92b6660c2557cf88ea53659
Rakibul Islam <rakibul241221@gmail.com>
2022-09-27 17:23:38 +0600

    cart summery added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './Cart.css'
 const Cart = ({ cart }) => {
+    let price = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        price = price + product.price * product.quantity;
+        shipping += product.shipping;
+    }
+    const tax = parseFloat((price * .075).toFixed(2));
+    const grandTotal = price + shipping + tax;
     return (
         <div className='cart'>
             <h2>Cart Summery</h2>
-            <p>Selected Items: {cart.length}</p>
-            <p>Total Price: </p>
-            <p>Total Shipping: </p>
-            <p>Tax: </p>
-            <h5>Grand Total: </h5>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${price}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
